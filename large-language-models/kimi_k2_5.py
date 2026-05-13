d0f9a8af6b5bda172c83589e283911618233f673
Yusuf Olokoba <olokobayusuf@gmail.com>
2026-05-13 11:08:35 -0400

    Minor Kimi update

--- large-language-models/kimi_k2_5.py
+++ large-language-models/kimi_k2_5.py
@@ -79,7 +79,10 @@ with force_eager_attn(draft_config), suppress_init_weights(), init_empty_weights
 # Create the continuous batching manager
 generation_config = GenerationConfig(
     max_new_tokens=2048,
-    eos_token_id=tokenizer.eos_token_id,
+    eos_token_id=[
+        tokenizer.eos_token_id,
+        tokenizer.convert_tokens_to_ids("<|im_end|>")
+    ],
     pad_token_id=tokenizer.pad_token_id,
     do_sample=True,
     temperature=0.7,
